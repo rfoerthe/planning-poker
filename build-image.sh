09f89720cbf285f298f54c1637531d6247b8332d
Roland Foerther <roland@foerther.de>
2026-01-01 20:18:25 +0100

    refactor: simplify cleanup commands in build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git pull
-rm -rf dist/*
-rm -rf node_modules/*
+rm -rf dist
+rm -rf node_modules
 pnpm install && pnpm dlx update-browserslist-db@latest && docker build -t planning-poker:latest .
